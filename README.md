a3d5b107e91bd04a903b78e89cb50ca2a22aa9f9
tahiry-dev <tahirycontact@gmail.com>
2022-07-16 16:20:18 +0300

    write all necessary code to launch to app

---
+++ README.md
@@ -0,0 +1,59 @@
+# Todo React Typescript 
+
+> The <b>Todo React Typescript</b> is an application where you can use to add or update your
+daily tasks. The main purpose of this application is practicing React with Typescript
+
+## App functionality
+
+Just create a title of your todo, describe it with a sentence and choose between 3 different
+status, Todo, Doing or Done
+
+## Screenshot of the app.
+
+
+## Built With
+
+- React.js
+- create-react-app
+- AntDesign
+
+
+## Prerequisities
+
+To get this project up and running locally, you must have [node](https://nodejs.org/en/)
+
+## Getting Started
+
+**To get this project set up on your local machine, follow these simple steps:**
+
+**Step 1**<br>
+Navigate through the local folder where you want to clone the repository and run<br>
+`git clone https://github.com/tahiry-dev/react-todo.git`. It will clone the repo to your local folder.<br>
+**Step 2**<br>
+get inside the local folder where you cloned the repository<br>
+**Step 3**<br>
+Run `npm install` to install the npm packages from the `package.json` file.<br>
+**Step 4**<br>
+Run `npm start` to start the webpack server, you can now navigate to `http://localhost:3000` to view the app. The server refreshes the app every time you make a change to a file used by it.<br>
+
+## Authors
+
+👤 **RANDRIAMIARINTSOA Tahiry**
+
+- Github: [tahiry_dev](https://github.com/tahiry-dev)
+- Linkedin: [Randriamiarintsoa](https://www.linkedin.com/in/tahiry-randriamiarintsoa/)
+
+## 🤝 Contributing
+
+Our favourite contributions are those that help us improve the project, whether with a contribution, an issue, or a feature request!
+
+## Show your support
+
+If you've read this far....give us a ⭐️!
+
+## 📝 License
+
+This project is licensed by Microverse and the Odin Project
+
+## Credits
+Thanks to Mr Nyhasina From HEI 
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,165 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Button, Form, Input, Select, Card, Col, Row, Divider } from 'antd';
+import 'antd/dist/antd.css';
 import './App.css';
 
-function App() {
+
+const { Option } = Select;
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 10 },
+};
+
+const tailLayout = {
+  wrapperCol: { offset: 11, span: 9 },
+};
+
+
+interface ITask {
+  title: string;
+  description: string;
+}
+
+
+const App: React.FC = () => {
+  const [form] = Form.useForm();
+
+  const [dataTodo, setDataTodo] = useState<ITask[]>([]);
+  const [dataDoing, setDataDoing] = useState<ITask[]>([]);
+  const [dataDone, setDataDone] = useState<ITask[]>([]);
+  const [updateButtonState, setUpdateButtonState] = useState(true);
+  const [submitButtonState, setSubmitButtonState] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const onFinish = (values: any) => {
+    addTask(values.title, values.description, values.status);
+    form.resetFields();
+  };
+
+  const addTask = (title: string, description: string, status: string) => {
+    if(status === "todo"){
+      const newTodo: ITask[] = [...dataTodo, { title, description }]
+      setDataTodo(newTodo);
+    }else if(status === "doing"){
+      const newDoing: ITask[] = [...dataDoing, { title, description }]
+      setDataDoing(newDoing);
+    }else {
+      const newDone: ITask[] = [...dataDone, { title, description }]
+      setDataDone(newDone);
+    }
+  }
+
+  const addUpdate = (title: string, description: string, status: string) => {
+  
+    if(status === "todo"){
+      dataTodo[currentIndex].title = title;
+      dataTodo[currentIndex].description = description;
+    }else if(status === "doing"){
+      dataDoing[currentIndex].title = title;
+      dataDoing[currentIndex].description = description;
+    }else {
+      dataDone[currentIndex].title = title;
+      dataDone[currentIndex].description = description;
+    }
+  }
+
+  const handleCardClick = (e: any, key: number) => {
+    setCurrentIndex(key);
+
+    let dataToUpdate;
+
+    if(e.currentTarget.title === "todo"){
+      dataToUpdate = dataTodo[key];
+    }else if(e.currentTarget.title === "doing"){
+      dataToUpdate = dataDoing[key];
+    }else{
+      dataToUpdate = dataDone[key];
+    }
+    
+    setUpdateButtonState(false);
+    setSubmitButtonState(true);
+    form.setFieldsValue({ title: dataToUpdate?.title, description: dataToUpdate?.description, status: e.currentTarget.title});
+  }
+
+  const handleUpdate =(values: any) => {
+      addUpdate(values.title, values.description, values.status)
+      setUpdateButtonState(true);
+      setSubmitButtonState(false);
+      form.resetFields();
+  }
+
+
   return (
-    <div className="App">
-      
-    </div>
+      <div className="App">
+        <Form {...layout} form={form} name="control-hooks" onFinish={ updateButtonState ? onFinish : handleUpdate}>
+          <Form.Item name="title" label="Title" rules={[{ required: true }]}>
+            <Input />
+          </Form.Item>
+
+          <Form.Item name="description" label="Description" rules={[{ required: true }]}>
+            <Input />
+          </Form.Item>
+
+          <Form.Item name="status" label="Status"  rules={[{ required: true }]}>
+            <Select
+              placeholder="Please choose your status"
+              allowClear
+            >
+              <Option  value="todo">Todo</Option>
+              <Option value="doing">Doing</Option>
+              <Option value="done">Done</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit" disabled={submitButtonState}>
+              Add Tasks
+            </Button>
+
+            <Button className="update-button" type="primary" htmlType='submit' disabled={updateButtonState} >
+               Update Tasks
+            </Button>
+          </Form.Item>
+        </Form>
+
+        <div className="site-card-wrapper">
+          <Row gutter={16}>
+            <Col span={8}>
+              <Card title="TODO" bordered={false}>
+                {
+                  dataTodo.map((task: ITask, index: number) => (
+                  <div key={index} title={"todo"} onClick={e => handleCardClick(e, index)}>
+                    <p><b>Title:</b> {task.title} <br /> <b>Description:</b> {task.description}</p>
+                    <Divider />
+                  </div>
+                ))}
+              </Card>
+            </Col>
+            <Col span={8}>
+              <Card title="DOING" bordered={false}>
+                {
+                  dataDoing.map((task: ITask, index: number) => (
+                  <div key={index} title={"doing"} onClick={e => handleCardClick(e, index)}>
+                    <p key={index}><b>Title:</b> {task.title} <br /> <b>Description:</b> {task.description}</p>
+                    <Divider />
+                  </div>
+                ))}
+              </Card>
+            </Col>
+            <Col span={8}>
+              <Card title="DONE" bordered={false}>
+                {
+                  dataDone.map((task: ITask, index: number) => (
+                  <div key={index} title={"done"} onClick={e => handleCardClick(e, index)}>
+                    <p key={index}><b>Title:</b> {task.title} <br /> <b>Description:</b> {task.description}</p>
+                    <Divider />
+                  </div>
+                ))}
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </div>
   );
 }
 
